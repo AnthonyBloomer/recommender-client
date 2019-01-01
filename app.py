70d15adb85e2eb6ee18c9983549a13423aa63d0e
Anthony Bloomer <ant0@protonmail.ch>
2019-01-01 18:05:51 +0000

    Update

--- app.py
+++ app.py
@@ -3,9 +3,54 @@ from recommender.api import Recommender
 import requests
 import os
 import json
-import newrelic
+import newrelic.agent
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def home():
+    music_genres = load_genres()['genres']
+    return render_template('index.html', genres=music_genres)
+
+
+@app.route("/genres", methods=['POST'])
+def genres():
+    music_genres = load_genres()
+    return jsonify(music_genres['genres'])
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    newrelic.agent.record_exception(e)
+    return jsonify({
+        'error': "Bad Request"
+    })
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    newrelic.agent.record_exception(e)
+    return jsonify({
+        'error': "Page not Found"
+    })
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    newrelic.agent.record_exception(e)
+    return jsonify({
+        'error': "Method not allowed."
+    })
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    newrelic.agent.record_exception(e)
+    return jsonify({
+        'error': "Internal server error."
+    })
+
 
 def load_genres():
     with open('resources/genres.json') as f:
@@ -13,11 +58,22 @@ def load_genres():
     return data
 
 
-def make_request(url, params):
-    req = requests.get(url, params=params)
-    if not req.ok:
-        print(req.content)
-    return req.json()
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+    recommender = Recommender()
+    if request.args.get('q'):
+        q = request.args.get('q')
+        t = request.args.get('t')
+        if t == 'artist':
+            recommender.artists = q
+        elif t == 'track':
+            recommender.tracks = q
+        else:
+            recommender.genres = q
+    recs = recommender.find_recommendations()
+    if 'error' in recs:
+        print(recs)
+    return jsonify(recs)
 
 
 @app.route('/play', methods=['POST'])
@@ -40,62 +96,11 @@ def play():
     })
 
 
-@app.route('/recommendations', methods=['POST'])
-def recommendations():
-    recommender = Recommender()
-    if request.args.get('q'):
-        q = request.args.get('q')
-        t = request.args.get('t')
-        if t == 'artist':
-            recommender.artists = q
-        elif t == 'track':
-            recommender.tracks = q 
-        else:
-            recommender.genres = q
-    recs = recommender.find_recommendations()
-    if 'error' in recs:
-        print(recs)
-    return jsonify(recs)
-
-
-@app.route("/genres", methods=['POST'])
-def genres():
-    music_genres = load_genres()['genres']
-    return jsonify(music_genres)
-
-
-@app.route("/", methods=['GET'])
-def home():
-    music_genres = load_genres()['genres']
-    return render_template('index.html', genres=music_genres)
-
-
-@app.errorhandler(400)
-def bad_request(e):
-    return jsonify({
-        'error': "Bad Request"
-    })
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return jsonify({
-        'error': "Page not Found"
-    })
-
-
-@app.errorhandler(405)
-def method_not_allowed(e):
-    return jsonify({
-        'error': "Method not allowed."
-    })
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return jsonify({
-        'error': "Internal server error."
-    })
+def make_request(url, params):
+    req = requests.get(url, params=params)
+    if not req.ok:
+        print(req.content)
+    return req.json()
 
 
 if __name__ == '__main__':
