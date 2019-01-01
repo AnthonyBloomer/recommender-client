c7acef042e1337aacef097293588ac2a23c4bcf1
Anthony Bloomer <ant0@protonmail.ch>
2019-01-01 03:58:36 +0000

    Update

--- app.py
+++ app.py
@@ -13,24 +13,30 @@ def load_genres():
     return data
 
 
+def make_request(url, params):
+    req = requests.get(url, params=params)
+    if not req.ok:
+        print(req.content)
+    return req.json()
+
+
 @app.route('/play', methods=['POST'])
 def play():
     if not request.args.get('track'):
         return jsonify({
             'error': 'Track is required.'
         }), 400
-    params = {
+
+    get_video_id = make_request('https://www.googleapis.com/youtube/v3/search', {
         'q': request.args.get('track'),
         'part': 'id,snippet',
         'maxResults': 1,
         'type': 'video',
         'key': os.getenv('YOUTUBE_API_KEY')
-    }
+    })
 
-    req = requests.get('https://www.googleapis.com/youtube/v3/search', params=params)
-    json = req.json()
     return jsonify({
-        "id": json['items'][0]['id']['videoId']
+        "id": get_video_id['items'][0]['id']['videoId']
     })
 
 
@@ -55,25 +61,52 @@ def recommendations():
         'max_popularity': request.args.get('max_popularity'),
     }
 
-    recommendations = recommender.find_recommendations()
-    if 'error' in recommendations:
-        print(recommendations)
-
-    return jsonify(recommendations)
+    recs = recommender.find_recommendations()
+    if 'error' in recs:
+        print(recs)
+    return jsonify(recs)
 
 
 @app.route("/genres", methods=['POST'])
 def genres():
-    genres = load_genres()['genres']
-    return jsonify(genres)
+    music_genres = load_genres()['genres']
+    return jsonify(music_genres)
 
 
 @app.route("/", methods=['GET'])
 def home():
-    genres = load_genres()['genres']
-    return render_template('index.html', genres=genres)
+    music_genres = load_genres()['genres']
+    return render_template('index.html', genres=music_genres)
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return jsonify({
+        'error': "Bad Request"
+    })
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({
+        'error': "Page not Found"
+    })
+
+
+@app.errorhandler(405)
+def page_not_found(e):
+    return jsonify({
+        'error': "Message not allowed."
+    })
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return jsonify({
+        'error': "Internal server error."
+    })
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
